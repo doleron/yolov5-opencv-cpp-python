9615c828a91c9a833ce00d2637124e5b0887a081
AiEson <2368442877@qq.com>
2022-03-27 12:07:30 +0800

    update backend

--- python/yolo.py
+++ python/yolo.py
@@ -2,14 +2,20 @@ import cv2
 import time
 import sys
 import numpy as np
+import cupy as cp
 import webRtc
+from PIL import Image
 
 root = ""
-root = "/home/aieson/Code/A01/yolov5-opencv-cpp-python"  # wcs
+root = "/home/aieson/Code/A01/yolov5-opencv-cpp-python"
 
 # Configs
-video_path = root + "/sample.mp4"
+video_path = root + "/Videos/bigroad.mp4"
 onnx_path = root + "/config_files/best.onnx"
+class_file_path = "/config_files/classes.txt"
+
+# onnx_path = root + "/config_files/yolov5s.onnx"
+# class_file_path = "/config_files/classes_yolov5s.txt"
 is_cuda = False if cv2.cuda.getCudaEnabledDeviceCount() == 0 else True
 
 
@@ -18,12 +24,13 @@ def build_model(is_cuda):
     if is_cuda:
         print("Attempty to use CUDA")
         net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
-        net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+        net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA_FP16)
     else:
         print("Running on CPU")
         net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
         net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
     return net
+
 
 INPUT_WIDTH = 640
 INPUT_HEIGHT = 640
@@ -31,10 +38,12 @@ SCORE_THRESHOLD = 0.2
 NMS_THRESHOLD = 0.4
 CONFIDENCE_THRESHOLD = 0.4
 
+
 def detect(image, net):
     # print("Detecting...")
-    blob = cv2.dnn.blobFromImage(image, 1/255.0, (INPUT_WIDTH, INPUT_HEIGHT), swapRB=True, crop=False)
     start = time.time()
+    blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (INPUT_WIDTH, INPUT_HEIGHT), swapRB=True, crop=False)
+
     net.setInput(blob)
     preds = net.forward()
     end = time.time()
@@ -43,11 +52,12 @@ def detect(image, net):
 
 def load_capture():
     capture = cv2.VideoCapture(video_path)
+    capture.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
     return capture
 
 def load_classes():
     class_list = []
-    with open(root + "/config_files/classes.txt", "r") as f:
+    with open(root + class_file_path,  "r") as f:
         class_list = [cname.strip() for cname in f.readlines()]
     return class_list
 
@@ -63,7 +73,7 @@ def wrap_detection(input_image, output_data):
     image_width, image_height, _ = input_image.shape
 
     x_factor = image_width / INPUT_WIDTH
-    y_factor =  image_height / INPUT_HEIGHT
+    y_factor = image_height / INPUT_HEIGHT
 
     for r in range(rows):
         row = output_data[r]
@@ -74,12 +84,11 @@ def wrap_detection(input_image, output_data):
             _, _, _, max_indx = cv2.minMaxLoc(classes_scores)
             class_id = max_indx[1]
             if (classes_scores[class_id] > .25):
-
                 confidences.append(confidence)
 
                 class_ids.append(class_id)
 
-                x, y, w, h = row[0].item(), row[1].item(), row[2].item(), row[3].item() 
+                x, y, w, h = row[0].item(), row[1].item(), row[2].item(), row[3].item()
                 left = int((x - 0.5 * w) * x_factor)
                 top = int((y - 0.5 * h) * y_factor)
                 width = int(w * x_factor)
@@ -87,7 +96,7 @@ def wrap_detection(input_image, output_data):
                 box = np.array([left, top, width, height])
                 boxes.append(box)
 
-    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.25, 0.45) 
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.25, 0.45)
 
     result_class_ids = []
     result_confidences = []
@@ -101,16 +110,13 @@ def wrap_detection(input_image, output_data):
     return result_class_ids, result_confidences, result_boxes
 
 def format_yolov5(frame):
-
     row, col, _ = frame.shape
     _max = max(col, row)
     result = np.zeros((_max, _max, 3), np.uint8)
     result[0:row, 0:col] = frame
     return result
 
-
 colors = [(255, 255, 0), (0, 255, 0), (0, 255, 255), (255, 0, 0)]
-
 
 net = build_model(is_cuda)
 capture = load_capture()
@@ -123,11 +129,14 @@ fps = -1
 frame_width = int(capture.get(3))
 frame_height = int(capture.get(4))
 
-out = cv2.VideoWriter('output.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 30, (frame_width, frame_height))
+# out = cv2.VideoWriter('output.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 30, (frame_width, frame_height))
 
 while True:
 
     _, frame = capture.read()
+    _, frame = capture.read()
+    _, frame = capture.read()
+
     if frame is None:
         print("End of stream")
         break
@@ -136,7 +145,7 @@ while True:
     width = 1600
     r = width / float(w)
     dim = (width, int(h * r))
-    # frame = cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
+    frame = cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
 
     inputImage = format_yolov5(frame)
     outs = detect(inputImage, net)
@@ -147,29 +156,30 @@ while True:
     total_frames += 1
 
     for (classid, confidence, box) in zip(class_ids, confidences, boxes):
-         color = colors[int(classid) % len(colors)]
-         cv2.rectangle(frame, box, color, 2)
-         cv2.rectangle(frame, (box[0], box[1] - 20), (box[0] + box[2], box[1]), color, -1)
-         cv2.putText(frame, class_list[classid], (box[0], box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, .5, (0,0,0))
+        color = colors[int(classid) % len(colors)]
+        cv2.rectangle(frame, box, color, 2)
+        cv2.rectangle(frame, (box[0], box[1] - 20), (box[0] + box[2], box[1]), color, -1)
+        cv2.putText(frame, class_list[classid], (box[0], box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, .5, (0, 0, 0))
+
+        cv2.circle(frame, (box[0] + (box[2] // 2), box[1] + (box[3] // 2)), radius=10, color=color[1])
 
     if frame_count >= 30:
         end = time.time_ns()
         fps = 1000000000 * frame_count / (end - start)
         frame_count = 0
         start = time.time_ns()
-    
+
     if fps > 0:
         fps_label = "FPS: %.2f" % fps
         cv2.putText(frame, fps_label, (10, 25), cv2.FONT_HERSHEY_SIMPLEX, .5, (0, 0, 255), 2)
-
 
     # print("Writing frame...")
     # out.write(frame)
     # print("Done")
 
-    # cv2.imshow("output", frame)
+    cv2.imshow("output", frame)
     # webRtc.wrapvideo(frame, _)
-    if cv2.waitKey(1) > -1:
+    if cv2.waitKey(1) & 0xFF == ord('q'): # 按下 q
         print("finished by user")
         cv2.destroyAllWindows()
         break
